dc48c72bc5d817c6b0d80effb0f135ced12512e7
Fernando Stteffen <fernando.stteffen@gmail.com>
2021-05-29 10:37:21 -0300

    refactor: event listener to literals

--- game_functions.py
+++ game_functions.py
@@ -2,27 +2,46 @@ import sys
 import pygame
 
 
-def check_events(ship):
-    """ Respond to keyboard and mouse events """
-    for event in pygame.event.get():
+def key_event(event, ship):
+    
+    keyboard_buttons = {
+            pygame.K_RIGHT: move_ship(event, ship),
+            pygame.K_LEFT: move_ship(event, ship), 
+    }
+    
+    
+    # fire event if that exist
+    if event.key in keyboard_buttons.keys():
+        keyboard_buttons[event.key]
+        
+
+def move_ship(event, ship):
+    
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_RIGHT: 
+            ship.moving_right = True
+        else:
+            ship.moving_left = True
             
+    elif event.type == pygame.KEYUP:
+        if event.key == pygame.K_RIGHT:
+            ship.moving_right = False
+        else:
+            ship.moving_left = False
+        
+        
+        
+def check_events(ship):
+    """ Listen events """
+    for event in pygame.event.get():
+         
+        # Top bar exit button
         if event.type == pygame.QUIT:
             sys.exit()
-            
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-                    
-            if event.key == pygame.K_LEFT:
-                ship.moving_left = True
-            
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-                
-            if event.key == pygame.K_LEFT:
-                ship.moving_left = False
-
+        
+        # KEYBOARD and MOUSE shot event
+        elif event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
+            key_event(event, ship)
             
 
 def update_screen(game_configs, screen, ship):
