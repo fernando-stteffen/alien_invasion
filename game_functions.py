749190e31d1ff5c175120b541ab39612127d7d15
Fernando Stteffen <fernando.stteffen@gmail.com>
2021-05-29 09:51:23 -0300

    add: variables of speed_factor to configs

--- game_functions.py
+++ game_functions.py
@@ -6,30 +6,22 @@ def check_events(ship):
     """ Respond to keyboard and mouse events """
     for event in pygame.event.get():
             
-            if event.type == pygame.KEYDOWN:
-                print("event key:" + str(event.key))
-                print("pygame key:" + str(pygame.K_LEFT))
+        if event.type == pygame.QUIT:
+            sys.exit()
             
-            if event.type == pygame.QUIT:
-                sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = True
+                    
+            if event.key == pygame.K_LEFT:
+                ship.moving_left = True
             
-            elif event.type == pygame.KEYDOWN:
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = False
                 
-                if event.key == pygame.K_RIGHT:
-                    print("Moving to the right")
-                    ship.moving_right = True
-                
-                if event.key == pygame.K_LEFT:
-                    print("Moving to the left")
-                    ship.moving_left = True
-            
-            elif event.type == pygame.KEYUP:
-                
-                if event.key == pygame.K_RIGHT:
-                    ship.moving_right = False
-                
-                if event.key == pygame.K_LEFT:
-                    ship.moving_left = False
+            if event.key == pygame.K_LEFT:
+                ship.moving_left = False
 
             
 
