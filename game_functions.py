eafaaa6493eb9f818fdc4a3b7435a3af7fb68085
Fernando Stteffen <fernando.stteffen@gmail.com>
2021-05-30 08:27:53 -0300

    fix/add: fix event caller, add bullet call to stack

--- game_functions.py
+++ game_functions.py
@@ -1,21 +1,29 @@
 import sys
 import pygame
+from bullet import Bullet
 
 
-def key_event(event, ship):
+def key_event(configs, screen, event, ship, bullets):
     
     keyboard_buttons = {
-            pygame.K_RIGHT: move_ship(event, ship),
-            pygame.K_LEFT: move_ship(event, ship), 
+            pygame.K_SPACE: shot_fire,
+            pygame.K_RIGHT: move_ship,
+            pygame.K_LEFT: move_ship, 
     }
-    
     
     # fire event if that exist
     if event.key in keyboard_buttons.keys():
-        keyboard_buttons[event.key]
-        
+        keyboard_buttons[event.key](configs, screen, event, ship, bullets)
+   
 
-def move_ship(event, ship):
+def shot_fire(configs, screen, event, ship, bullets):
+    if event.type == pygame.KEYDOWN: 
+        new_bullet = Bullet(configs, screen, ship)
+        bullets.add(new_bullet)
+    
+    
+
+def move_ship(configs, screen, event, ship, bullets):
     
     if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_RIGHT: 
@@ -31,7 +39,7 @@ def move_ship(event, ship):
         
         
         
-def check_events(ship):
+def check_events(configs, screen, ship, bullets):
     """ Listen events """
     for event in pygame.event.get():
          
@@ -41,14 +49,17 @@ def check_events(ship):
         
         # KEYBOARD and MOUSE shot event
         elif event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
-            key_event(event, ship)
+            key_event(configs, screen, event, ship, bullets)
             
 
-def update_screen(game_configs, screen, ship):
+def update_screen(game_configs, screen, ship, bullets):
     """ Update sprites in the new screen """
     
     # Dawing
     screen.fill(game_configs.background_color)
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
+        
     ship.blitme()
 
     # Refresh Screen
