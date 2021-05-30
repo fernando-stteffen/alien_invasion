aab9c9175fee3f1e32348ad89ff848d75b147e3b
Fernando Stteffen <fernando.stteffen@gmail.com>
2021-05-30 09:56:26 -0300

    refactoring: game_functions ordering

--- game_functions.py
+++ game_functions.py
@@ -11,8 +11,50 @@ def set_globals(configs, screen):
     global game_screen
     game_configs = configs
     game_screen = screen
+            
+
+        
+def update_screen(ship, bullets):
+    """ Update sprites in the new screen """
     
+    # Dawing
+    game_screen.fill(game_configs.background_color)
     
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
+        
+    ship.blitme()
+
+    # Refresh Screen
+    pygame.display.flip()  
+
+    
+
+def update_bullets(bullets):
+    """Update position of bullets and ger rid of old bullets."""
+    
+    bullets.update()
+    
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+            
+            
+        
+def check_events(ship, bullets):
+    """ Listen events """
+    for event in pygame.event.get():
+         
+        # Top bar exit button
+        if event.type == pygame.QUIT:
+            sys.exit()
+        
+        # KEYBOARD and MOUSE shot event
+        elif event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
+            key_event(event, ship, bullets)
+            
+            
+
 def key_event(event, ship, bullets):
     keyboard_buttons = {
             pygame.K_SPACE: shot_fire,
@@ -24,7 +66,7 @@ def key_event(event, ship, bullets):
     # fire event if that exist
     if event.key in keyboard_buttons.keys():
         keyboard_buttons[event.key](event, ship, bullets)
-   
+
 
 def close_game(*variables): 
     sys.exit()
@@ -50,46 +92,3 @@ def move_ship(event, ship, bullets):
             ship.moving_right = False
         else:
             ship.moving_left = False
-        
-        
-        
-def check_events(ship, bullets):
-    """ Listen events """
-    for event in pygame.event.get():
-         
-        # Top bar exit button
-        if event.type == pygame.QUIT:
-            sys.exit()
-        
-        # KEYBOARD and MOUSE shot event
-        elif event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
-            key_event(event, ship, bullets)
-            
-
-def update_screen(ship, bullets):
-    """ Update sprites in the new screen """
-    
-    # Dawing
-    game_screen.fill(game_configs.background_color)
-    
-    for bullet in bullets.sprites():
-        bullet.draw_bullet()
-        
-    ship.blitme()
-
-    # Refresh Screen
-    pygame.display.flip()  
-    
-
-def update_bullets(bullets):
-    """Update position of bullets and ger rid of old bullets."""
-    
-    bullets.update()
-    
-    for bullet in bullets.copy():
-        if bullet.rect.bottom <= 0:
-            bullets.remove(bullet)
-
-    
-    
-    
