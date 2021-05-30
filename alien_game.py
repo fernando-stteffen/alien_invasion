922cbaba2d3856d6bcee955032045b69b7bd89b8
Fernando Stteffen <fernando.stteffen@gmail.com>
2021-05-30 09:54:17 -0300

    refactoring: game_functions add globals game and configs

--- alien_game.py
+++ alien_game.py
@@ -5,9 +5,12 @@ from pygame.sprite import Group
 from config import Configs
 from ship import Ship
 
+
+
 def run_game():
         
     configs = Configs()
+    
     
     pygame.init()
     pygame.display.set_caption(configs.get_game_name())
@@ -16,13 +19,14 @@ def run_game():
     ship = Ship(configs, screen)
     bullets = Group()
     
-
+    game_functions.set_globals(configs, screen)
+    
     while True:
-            
-        game_functions.check_events(configs, screen, ship, bullets)
+        
+        game_functions.check_events(ship, bullets)
         ship.update()
         game_functions.update_bullets(bullets)
-        game_functions.update_screen(configs, screen, ship, bullets)
+        game_functions.update_screen(ship, bullets)
 
 
 
--- game_functions.py
+++ game_functions.py
@@ -3,28 +3,41 @@ import pygame
 from bullet import Bullet
 
 
+game_configs = ()
+game_screen = ()
+
+def set_globals(configs, screen):
+    global game_configs
+    global game_screen
+    game_configs = configs
+    game_screen = screen
     
     
-def key_event(configs, screen, event, ship, bullets):
+def key_event(event, ship, bullets):
     keyboard_buttons = {
             pygame.K_SPACE: shot_fire,
             pygame.K_RIGHT: move_ship,
-            pygame.K_LEFT: move_ship, 
+            pygame.K_LEFT: move_ship,
+            pygame.K_q: close_game, 
     }
     
     # fire event if that exist
     if event.key in keyboard_buttons.keys():
-        keyboard_buttons[event.key](configs, screen, event, ship, bullets)
+        keyboard_buttons[event.key](event, ship, bullets)
    
 
-def shot_fire(configs, screen, event, ship, bullets):
-    if event.type == pygame.KEYDOWN and len(bullets) < configs.max_bullets: 
-        new_bullet = Bullet(configs, screen, ship)
+def close_game(*variables): 
+    sys.exit()
+    
+    
+def shot_fire(event, ship, bullets):
+    if event.type == pygame.KEYDOWN and len(bullets) < game_configs.max_bullets: 
+        new_bullet = Bullet(game_configs, game_screen, ship)
         bullets.add(new_bullet)
     
     
 
-def move_ship(configs, screen, event, ship, bullets):
+def move_ship(event, ship, bullets):
     
     if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_RIGHT: 
@@ -40,7 +53,7 @@ def move_ship(configs, screen, event, ship, bullets):
         
         
         
-def check_events(configs, screen, ship, bullets):
+def check_events(ship, bullets):
     """ Listen events """
     for event in pygame.event.get():
          
@@ -50,14 +63,15 @@ def check_events(configs, screen, ship, bullets):
         
         # KEYBOARD and MOUSE shot event
         elif event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
-            key_event(configs, screen, event, ship, bullets)
+            key_event(event, ship, bullets)
             
 
-def update_screen(configs, screen, ship, bullets):
+def update_screen(ship, bullets):
     """ Update sprites in the new screen """
     
     # Dawing
-    screen.fill(configs.background_color)
+    game_screen.fill(game_configs.background_color)
+    
     for bullet in bullets.sprites():
         bullet.draw_bullet()
         
@@ -75,8 +89,7 @@ def update_bullets(bullets):
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
-        
-    print(len(bullets))
+
     
     
     
