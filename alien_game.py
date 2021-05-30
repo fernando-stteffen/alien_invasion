26411ac8e04a6bd99b2f6e1a3c8ac532c42cd22d
Fernando Stteffen <fernando.stteffen@gmail.com>
2021-05-30 08:26:14 -0300

    change: pass variables configs and screen to check_events

--- alien_game.py
+++ alien_game.py
@@ -1,6 +1,7 @@
 import pygame
 import game_functions 
 
+from pygame.sprite import Group
 from config import Configs
 from ship import Ship
 
@@ -14,13 +15,16 @@ def run_game():
     
     ship = Ship(configs, screen)
 
+    bullets = Group()
+
     while True:
 
-        game_functions.check_events(ship)
+        game_functions.check_events(configs, screen, ship, bullets)
         
         ship.update()
+        bullets.update()
         
-        game_functions.update_screen(configs, screen, ship)
+        game_functions.update_screen(configs, screen, ship, bullets)
 
 
 
