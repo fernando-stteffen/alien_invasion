3a1c1e1360fbfac1d5b79336a24826754420de53
Fernando Stteffen <fernando.stteffen@gmail.com>
2021-05-30 10:42:53 -0300

    add: created alien group and called function to create row of aliens

--- alien_game.py
+++ alien_game.py
@@ -8,25 +8,35 @@ from ship import Ship
 
 
 def run_game():
-        
+    
+    #Load Configs    
     configs = Configs()
     
-    
+    #Create Window
     pygame.init()
     pygame.display.set_caption(configs.get_game_name())
     screen = pygame.display.set_mode(configs.get_display_size())
         
+    
+    #Elements    
     ship = Ship(configs, screen)
     bullets = Group()
+    aliens = Group()
     
+    
+    # Set Configs
     game_functions.set_globals(configs, screen)
+    game_functions.create_fleet(aliens)
     
     while True:
         
+        #Listen
         game_functions.check_events(ship, bullets)
+        
+        # Upadating Elements
         ship.update()
         game_functions.update_bullets(bullets)
-        game_functions.update_screen(ship, bullets)
+        game_functions.update_screen(ship, bullets, aliens)
 
 
 
